eeccde46628de7ac00b00d9aa6af8d0b165145aa
Maciej Kozak <kozakmaciej200@gmail.com>
2022-10-27 12:40:34 +0200

    routing, beginning

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
-<?php
+<?php  //index.php jest pierwszym plikiem uruchamianym na serwerze 
 
-echo "Hello world!!";
+require 'Routing.php'; //importujemy 
+
+$path = trim($_SERVER['REQUEST_URI'], '/'); //tworzymy sciezke ktorą wyslala nam przegladarka na serwer, pobieramy ja za pomoca zmiennej %_SERVER['klucz'], trim bo chce sie pozbyc /
+$path = parse_url( $path, PHP_URL_PATH); //parsujemy 
+//Logika: w momencie wywolania urla index albo register itd otwieramy sobie metode index z DefaultControllera 
+//Router::get('index', 'DefaultController'); //wrzucamy sciezki do metody get, ktora zaimplementowalismy w pliku Routing.php
+//Router::get('register', 'DefaultController');
+Router::get('first', 'DefaultController');
+//Router::post('login', 'SecurityController');
+//Router::post('addRecipe', 'RecipeController');
+Router::get('cook', 'DefaultController');
+//Router::get('recipes', 'RecipeController');
+Router::get('share', 'DefaultController');
+//Router::post('search', 'RecipeController');
+//Router::get('like', 'RecipeController');
+//Router::get('dislike', 'RecipeController');
+Router::get('admin', 'DefaultController');
+Router::get('settings', 'DefaultController');
+
+//Router::post('adduser', 'SecurityController');
+//Router::post('logout', 'SecurityController');
+
+Router::run($path); //wywolanie metody run, jako argument sciezka 
