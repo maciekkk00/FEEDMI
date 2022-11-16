1ad95e02104adc5287ae7f9c60bac2cfbdc62994
Maciej Kozak <kozakmaciej200@gmail.com>
2022-11-16 18:08:06 +0100

    javascript_to_fix

--- index.php
+++ index.php
@@ -5,21 +5,21 @@ require 'Routing.php'; //importujemy
 $path = trim($_SERVER['REQUEST_URI'], '/'); //tworzymy sciezke ktorą wyslala nam przegladarka na serwer, pobieramy ja za pomoca zmiennej %_SERVER['klucz'], trim bo chce sie pozbyc /
 $path = parse_url( $path, PHP_URL_PATH); //parsujemy 
 //Logika: w momencie wywolania urla index albo register itd otwieramy sobie metode index z DefaultControllera 
-//Router::get('index', 'DefaultController'); //wrzucamy sciezki do metody get, ktora zaimplementowalismy w pliku Routing.php
-//Router::get('register', 'DefaultController');
+Router::get('index', 'DefaultController'); //wrzucamy sciezki do metody get, ktora zaimplementowalismy w pliku Routing.php
+Router::get('register', 'DefaultController');
 Router::get('first', 'DefaultController');
 Router::post('login', 'SecurityController'); //tutaj wyznaczyliśmy, że pod url login ma zostac otwarty SecurityController z akcja login
 Router::post('addRecipe', 'RecipeController');
 Router::get('cook', 'DefaultController');
-//Router::get('recipes', 'RecipeController');
+Router::get('recipes', 'RecipeController');
 Router::get('share', 'DefaultController');
-//Router::post('search', 'RecipeController');
-//Router::get('like', 'RecipeController');
-//Router::get('dislike', 'RecipeController');
+Router::post('search', 'RecipeController');
+Router::get('like', 'RecipeController');
+Router::get('dislike', 'RecipeController');
 Router::get('admin', 'DefaultController');
 Router::get('settings', 'DefaultController');
 
-//Router::post('adduser', 'SecurityController');
-//Router::post('logout', 'SecurityController');
+Router::post('adduser', 'SecurityController');
+Router::post('logout', 'SecurityController');
 
 Router::run($path); //wywolanie metody run, jako argument sciezka 
--- src/controllers/SecurityController.php
+++ src/controllers/SecurityController.php
@@ -2,7 +2,7 @@
 //kontroler do logowania, wylogowania i rejestracji
 require_once 'AppController.php';
 require_once __DIR__ .'/../models/User.php'; //importujemy dostęp do modelu
-//require_once __DIR__.'/../repository/UserRepository.php';
+require_once __DIR__.'/../repository/UserRepository.php';
 
 class SecurityController extends AppController {
 
