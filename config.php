aaac09fa653743ff4abdea9fe568395dcee7dc0e
Maciej Kozak <kozakmaciej200@gmail.com>
2023-03-30 12:17:26 +0200

    config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-const USERNAME = 'payvgtldxirxol';
-const PASSWORD = '10b8956e9b3da8f5d017fdf286e0aff8cd5c941ba6f65d5f809c314a5996ef64';
-const HOST = 'ec2-34-241-19-183.eu-west-1.compute.amazonaws.com';
-const DATABASE = 'd3fk97f6qh6e5c';
+const USERNAME = '';
+const PASSWORD = '';
+const HOST = '';
+const DATABASE = '';
